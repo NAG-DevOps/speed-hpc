59abecad301866a53e15b380c874c81d8254952c
yulgroper <79223724+yulgroper@users.noreply.github.com>
2021-11-11 11:54:14 -0500

    Steps to create environment for efficientdet
    # creating environment for efficientdet on speed
    
    # cd to speed_scratch
    cd /speed-scratch/<encs_username>
    
    # load python module
    module load python/3.8.3
    
    # create virtual environment
    python3 -m venv <environ_name>
    
    # activate virtual environment
    source <environ_name>/bin/activate.csh
    
    # install DL packages for Efficientdet
    pip install tensorflow==2.7.0
    pip install lxml>=4.6.1
    pip install absl-py>=0.10.0
    pip install matplotlib>=3.0.3
    pip install numpy>=1.19.4
    pip install Pillow>=6.0.0
    pip install PyYAML>=5.1
    pip install six>=1.15.0
    pip install tensorflow-addons>=0.12
    pip install tensorflow-hub>=0.11
    pip install neural-structured-learning>=1.3.1
    pip install tensorflow-model-optimization>=0.5
    pip install Cython>=0.29.13
    pip install git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI

---
+++ src/efficientdet.sh
@@ -0,0 +1,40 @@
+#!/encs/bin/tcsh
+
+##
+## This script was submitted by a member of Dr. Amer's Research Group
+##
+
+##
+## Prerequisite: 
+##  Efficientdet Virtual Environment (see efficientdet_virtual_env_speed.txt)
+##
+
+##
+## Job Scheduler options 
+##
+
+#$ -N efficientdet_pascal
+#$ -cwd      
+#$ -pe smp 8
+#$ -l h_vmem=128G
+#$ -l gpu=2
+
+cd /speed-scratch/<encs_username>
+
+module load python/3.8.3
+module load cuda/11.5
+source envs/tf/bin/activate.csh
+
+cd code/automl/efficientdet
+
+python3 main.py --mode=train_and_eval \
+    --train_file_pattern=tfrecord/'pascal-*-of-00100.tfrecord' \
+    --val_file_pattern=tfrecord/'val-*-of-00032.tfrecord' \
+    --model_name='efficientdet-d0' \
+    --model_dir=model_path  \
+    --backbone_ckpt='efficientnet-b0' \
+    --train_batch_size=4 \
+    --eval_batch_size=4 --eval_samples=4952  \
+    --num_examples_per_epoch=16551  --num_epochs=300  \
+    --hparams="num_classes=20,moving_average_decay=0,mixed_precision=true" \
+    --strategy='gpus'
