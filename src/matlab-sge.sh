c08fdc9b25cf6cc39a28e1cab34d2180f71b3e98
carlos-encs <110119864+carlos-encs@users.noreply.github.com>
2022-08-01 09:56:17 -0400

    Update matlab-sge.sh

--- src/matlab-sge.sh
+++ src/matlab-sge.sh
@@ -20,9 +20,8 @@ module list
 
 # Matlab parameters - add/modify/remove them according your script
 
-# -singleCompThread: Forces MATLAB to run single-threaded
 # -nodesktop -nodisplay: Launches Matlab in the terminal
 # -nojvm : Tells Matlab to run without the Java Virtual Machine to reduce overhead
 # test.m : Matlab commands file
 
-matlab -singleCompThread -nodisplay -nodesktop -nojvm < test.m
+matlab -nodisplay -nodesktop -nojvm < test.m
