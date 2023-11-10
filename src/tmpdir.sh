c1f9deb2c75cdc9cdac9cf1c572db32a94d1c084
Serguei Mokhov <mokhov@cse.concordia.ca>
2023-11-10 08:49:21 -0500

    [src][tmpdir] fix var for cpus per task, add srun; misc fixes

--- src/tmpdir.sh
+++ src/tmpdir.sh
@@ -2,16 +2,16 @@
 
 #SBATCH --job-name=tmpdir      ## Give the job a name
 #SBATCH --mail-type=ALL        ## Receive all email type notifications
-#SBATCH --mail-user=$USER@encs.concordia.ca
+#SBATCH --mail-user=$USER
 #SBATCH --chdir=./             ## Use currect directory as working directory
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8      ## Request 8 cpus
+#SBATCH --cpus-per-task=8      ## Request 8 cores
 #SBATCH --mem=32G              ## Assign 32G memory per node 
 
 cd $TMPDIR
 mkdir input
 rsync -av $SLURM_SUBMIT_DIR/references/ input/
 mkdir results
-STAR --inFiles $TMPDIR/input --parallel $SLURM_NTASKS --outFiles $TMPDIR/results
+srun STAR --inFiles $TMPDIR/input --parallel $SRUN_CPUS_PER_TASK --outFiles $TMPDIR/results
 rsync -av $TMPDIR/results/ $SLURM_SUBMIT_DIR/processed/
