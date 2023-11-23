ff2a27c267b9fda87912e747fc53002d83aa20d0
Serguei Mokhov <mokhov@cse.concordia.ca>
2023-11-22 20:10:40 -0500

    [src][array] fix a typo and add --mem

--- src/array.sh
+++ src/array.sh
@@ -5,12 +5,13 @@
 #SBATCH -N 1
 #SBATCH -t 0-2:00
 #SBATCH --array=1-30
+#SBATCH --mem=1G
 #SBATCH -o myprogram%A_%a.out
 # %A" is replaced by the job ID and "%a" with the array index
 #SBATCH -e myprogram%A_%a.err
 
 echo "Would be input shard: input$SLURM_ARRAY_TASK_ID.dat"
-#/myprogram input$SLURM_ARRAY_TASK_ID.dat
+#./myprogram input$SLURM_ARRAY_TASK_ID.dat
 
 sleep 10
 
