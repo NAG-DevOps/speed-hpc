930a5f12d89564c0a25a80b96d070776fbe92175
carlos-encs <110119864+carlos-encs@users.noreply.github.com>
2024-05-03 12:18:28 -0400

    Update firsttime.sh -y option

--- src/jupyterlabs/firsttime.sh
+++ src/jupyterlabs/firsttime.sh
@@ -3,7 +3,7 @@ module load anaconda3/2023.03/default
 setenv TMPDIR /speed-scratch/$USER/tmp
 setenv TMP /speed-scratch/$USER/tmp
 setenv CONDA_PKGS_DIRS /speed-scratch/$USER/Jupyter/pkgs
-conda create -p /speed-scratch/$USER/Jupyter/jupyter-env
+conda create -p /speed-scratch/$USER/Jupyter/jupyter-env -y
 conda activate /speed-scratch/$USER/Jupyter/jupyter-env
-conda install -c conda-forge jupyterlab
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+conda install -c conda-forge jupyterlab -y
+pip3 install -y torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
