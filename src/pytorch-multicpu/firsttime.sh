24f7cf0790e3ad5d4c0ebf29ccb1d7d79d3b9988
Serguei Mokhov <mokhov@cse.concordia.ca>
2024-06-17 20:20:46 -0400

    [examples][pytorch-multicpu] adjust scripts per tests

--- src/pytorch-multicpu/firsttime.sh
+++ src/pytorch-multicpu/firsttime.sh
@@ -1,7 +1,9 @@
-#### Virtual ENV creation. One time only:
 #!/encs/bin/tcsh
+
+#### Virtual ENV creation. One time only.
+#### Run after salloc to a compute node!
+
 cd /speed-scratch/$USER
-salloc -p ps --mem=20Gb -A Your_group_id(if you have one)
 module load python/3.9.18/default
 setenv TMP /speed-scratch/$USER/tmp
 setenv TMPDIR /speed-scratch/$USER/tmp
@@ -10,4 +12,5 @@ source $TMPDIR/pytorchcpu/bin/activate.csh
 pip install torch torchvision
 pip install urllib3==1.26.6
 deactivate
-### END of VEnv creation
+
+#### END of VEnv creation
--- src/pytorch-multicpu/pytorch-multicpu.sh
+++ src/pytorch-multicpu/pytorch-multicpu.sh
@@ -1,4 +1,5 @@
 #!/encs/bin/tcsh
+
 #SBATCH --nodes 1
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=4 # change this parameter to 2,4,6,... to see the effect on performance
@@ -7,7 +8,11 @@
 #SBATCH --time=0:05:00
 #SBATCH --output=%N-%j.out
 
+#SBATCH --mail-type=ALL
+
 source /speed-scratch/$USER/tmp/pytorchcpu/bin/activate.csh
 echo "starting training..."
 
-time python torch-multicpu.py
+time srun python torch-multicpu.py
+
+# EOF
