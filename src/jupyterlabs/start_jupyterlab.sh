f2a5a37acdd6106596fb61968107d965a6187814
Serguei Mokhov <mokhov@cse.concordia.ca>
2024-10-04 11:32:06 -0400

    [src][start_jupyterlab] add mail-type and pt and some comments

--- src/jupyterlabs/start_jupyterlab.sh
+++ src/jupyterlabs/start_jupyterlab.sh
@@ -1,2 +1,3 @@
-srun --mem=16G --gpus=1 /speed-scratch/$USER/speed-hpc/src/jupyterlabs/run_jupyterlab.sh
-
+# Start either a part of interactive or batch sesions
+# Hardcoded assumption: /speed-scratch/$USER/speed-hpc/src/jupyterlabs
+srun --mem=16G --gpus=1 -p pt --mail-type=ALL /speed-scratch/$USER/speed-hpc/src/jupyterlabs/run_jupyterlab.sh
