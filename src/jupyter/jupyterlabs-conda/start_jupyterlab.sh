b28062f08b522208b8dd8fa7e53e9f745d37484a
Serguei Mokhov <mokhov@cse.concordia.ca>
2025-01-16 11:40:45 -0500

    [src][jupyterlabs-conda] fix up and document start_jupyterlab.sh

--- src/jupyter/jupyterlabs-conda/start_jupyterlab.sh
+++ src/jupyter/jupyterlabs-conda/start_jupyterlab.sh
@@ -1,3 +1,12 @@
 # Start either a part of interactive or batch sesions
-# Hardcoded assumption: /speed-scratch/$USER/speed-hpc/src/jupyterlabs
-srun --mem=16G --gpus=1 -p pt --mail-type=ALL /speed-scratch/$USER/speed-hpc/src/jupyterlabs/run_jupyterlab.sh
+# Hardcoded assumptions to adjust as necessary:
+#  path:          /speed-scratch/$USER/speed-hpc/src/jupyter/jupyterlabs-conda
+#  memory:        16GB
+#  GPUs:          1
+#  partition:     pt
+#  CPUs:          8
+#  notifications: ALL
+# If calling from sbatch, make sure these don't exceed those from sbatch
+srun \
+  --mem=16G --gpus=1 -p pt -c 8 --mail-type=ALL \
+  /speed-scratch/$USER/speed-hpc/src/jupyter/jupyterlabs-conda/run_jupyterlab.sh
