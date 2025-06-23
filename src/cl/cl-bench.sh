2237a29fe94ca997875204da0fc94d604064ac65
Serguei Mokhov <mokhov@cse.concordia.ca>
2025-06-23 10:23:25 -0400

    [src][cl] update cl-bench.sh to pull req repo and sbatch itself

--- src/cl/cl-bench.sh
+++ src/cl/cl-bench.sh
@@ -1,29 +1,65 @@
 #!/encs/bin/tcsh
 
+#
+# Runs an AMDGPU benchmark
+#
+
 #SBATCH -J cl-bench
-#SBATCH -A root
 #SBATCH --mem=50G
 #SBATCH -p cl
 #SBATCH -n 1
 #SBATCH -c 8
 #SBATCH -w speed-19
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=serguei
-## SBATCH --gpus=amdgpu:1
-## SBATCH --gres=gpu:amdgpu:1
+#SBATCH --gres=gpu:amdgpu:1
 #SBATCH --constraint=amdgpu
 
 echo "cl-bench START"
-
-date
-time srun /speed-scratch/serguei/cl/amd-gpu-benchmark/cl-info
-date
-time srun /speed-scratch/serguei/cl/amd-gpu-benchmark/main
-#srun strace /speed-scratch/serguei/cl/amd-gpu-benchmark/main
-#strace /speed-scratch/serguei/cl/amd-gpu-benchmark/main
-date
-time srun /speed-scratch/serguei/cl/amd-gpu-benchmark/main-cpu
 date
 
+if (! $?SLURM_JOB_ID ) then
+  echo "Did not detect Slurm job ID... running on"
+  hostname
+  echo "Will spin a job to a compute node..."
+  sbatch $0
+  exit 0
+else
+  echo -n "Running on "
+  hostname
+endif
 
+set REPO = "amd-gpu-benchmark"
+set GHBASE = "https://github.com/NAG-DevOps"
+
+if ( ! -d $REPO ) then
+  echo "Cloning $REPO repo..."
+  time srun git clone --depth=1 "$GHBASE"/"$REPO".git
+else
+  echo "Found COMP371_all already present; pulling in case of updates..."
+  pushd $REPO
+    time srun git pull --rebase --autostash
+  popd
+endif
+
+echo "$0 compiling..."
+date
+pushd $REPO
+  make  
+popd
+
+echo "$0 cl-info"
+date
+time srun ./$REPO/cl-info
+
+echo "$0 main"
+date
+time srun ./$REPO/main
+#srun strace ./$REPO/main
+#strace ./$REPO/main
+
+echo "$0 main-cpu"
+date
+time srun ./$REPO/main-cpu
+
+date
 echo "cl-bench DONE"
