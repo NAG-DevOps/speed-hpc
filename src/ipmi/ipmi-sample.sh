fe7011f5b1ea2cc1eea8ce9bcdb930498683373c
Serguei Mokhov <mokhov@cse.concordia.ca>
2025-07-04 10:10:28 -0400

    [ipmi] turn into a loop and add verbosity. RT542165, RT544326.

--- src/ipmi/ipmi-sample.sh
+++ src/ipmi/ipmi-sample.sh
@@ -1,30 +1,46 @@
 #!/encs/bin/bash
 
+# Sample sensor reader
+#
+# Assumes execution on speed-submit hosts:
+#  queries for an IMPI sensor value on each specified hosts as an srun task
+#  submits (or not) an sbatch heavy job if the value is below a treshold
+
 CPU_TEMP_TRESHOLD=50
 
-HOSTTOTEST="magic-node-01"
+# magic-node-[01-10] are in pm
+for host in magic-node-01 magic-node-02 magic-node-03 ; do
 
-# Reads an IMPI value off the node via srun and ipmitool
-CPU1_TEMP="$(srun -w $HOSTTOTEST -p pm --mem=10M --exact ipmitool sensor | grep 02-CPU | sed 's/ //g' | cut -d'|' -f2)"
+  HOSTTOTEST="$host"
 
-echo "CPU1_TEMP=$CPU1_TEMP"
-
-#if [[ $CPU1_TEMP -gt $CPU_TEMP_TRESHOLD ]]; then
-if (( $(echo "$CPU1_TEMP > $CPU_TEMP_TRESHOLD" | bc -l) )); then
-  echo "$HOSTTOTEST is too hot, $CPU1_TEMP > $CPU_TEMP_TRESHOLD, skipping load..."
+  echo "Processing $HOSTTOTEST"
   shownode $HOSTTOTEST
-else
-  echo "$CPU1_TEMP <= $CPU_TEMP_TRESHOLD"
-  echo "Allocating some workload..."
-  # Assuming the $USER clone the sample repo in /speed-scratch/$USER
-  pushd /speed-scratch/$USER/speed-hpc/src/gpaw
-    sbatch -p pm -w $HOSTTOTEST gpaw-container-less-constraints.sh
-    squeue -la -u $USER
+
+  # Reads an IMPI value off the node via srun and ipmitool
+  CPU1_TEMP="$(srun -w $HOSTTOTEST -p pm --mem=10M --exact ipmitool sensor | grep 02-CPU | sed 's/ //g' | cut -d'|' -f2)"
+
+  echo "CPU1_TEMP=$CPU1_TEMP"
+
+  if (( $(echo "$CPU1_TEMP > $CPU_TEMP_TRESHOLD" | bc -l) )); then
+    echo "$HOSTTOTEST is too hot, $CPU1_TEMP > $CPU_TEMP_TRESHOLD, skipping load..."
     shownode $HOSTTOTEST
-    CPU1_TEMP="$(srun -w $HOSTTOTEST -p pm --mem=10M --exact ipmitool sensor | grep 02-CPU | sed 's/ //g' | cut -d'|' -f2)"
-    echo "NOW CPU1_TEMP=$CPU1_TEMP"
-  popd
-fi
+  else
+    echo "$CPU1_TEMP <= $CPU_TEMP_TRESHOLD"
+    echo "Allocating some workload..."
+    # Assuming the $USER clone the sample repo in /speed-scratch/$USER
+    pushd /speed-scratch/$USER/speed-hpc/src/gpaw
+      sbatch -p pm -w $HOSTTOTEST gpaw-container-less-constraints.sh
+      squeue -la -u $USER
+      shownode $HOSTTOTEST
+      CPU1_TEMP="$(srun -w $HOSTTOTEST -p pm --mem=10M --exact ipmitool sensor | grep 02-CPU | sed 's/ //g' | cut -d'|' -f2)"
+      echo "NOW CPU1_TEMP=$CPU1_TEMP"
+    popd
+  fi
+
+done
+
+date
+echo "$0: done"
 
 # EOF
 
