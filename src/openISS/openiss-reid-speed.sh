757ac00493de9b1acffbae8e5618448f7c6156e9
Farah <49493059+salhanyf@users.noreply.github.com>
2025-07-17 17:20:28 -0400

    modified openiss-reid script

--- src/openISS/openiss-reid-speed.sh
+++ src/openISS/openiss-reid-speed.sh
@@ -23,8 +23,21 @@
 
 # Execute the script
 module load anaconda3/2023.03/default
-conda env create -f environment.yml -p /speed-scratch/$USER/reid-venv
-conda activate /speed-scratch/$USER/reid-venv
+
+# Define the environment directory
+set ENV_DIR = /speed-scratch/$USER/reid-venv
+mkdir -p $ENV_DIR/{tmp,pkgs,cache}
+
+setenv TMPDIR $ENV_DIR/tmp
+setenv TMP $ENV_DIR/tmp
+setenv CONDA_PKGS_DIRS $ENV_DIR/pkgs
+setenv PIP_CACHE_DIR $ENV_DIR/cache
+
+conda env create -f environment.yml -p $ENV_DIR >& conda-create.log
+conda activate $ENV_DIR
+
+date
 srun python reid.py
+date
+
 conda deactivate
-conda env remove -p /speed-scratch/$USER/reid-venv
