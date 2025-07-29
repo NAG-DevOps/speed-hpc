a93412616524a30071d52bd187f72fcf449b3b52
Farah <49493059+salhanyf@users.noreply.github.com>
2025-07-29 17:19:50 -0400

    update to reid script

--- src/openISS/openiss-reid-speed.sh
+++ src/openISS/openiss-reid-speed.sh
@@ -1,43 +1,42 @@
 #!/encs/bin/tcsh
 
-# Job name
 #SBATCH --job-name openiss-reid
+#SBATCH --mail-type=ALL                 ## Receive all email type notifications
+#SBATCH --chdir=./                      ## Use currect directory as working directory (default) 
+#SBATCH -o openiss-reid-output-%A.log   ## Specify output file name
 
-# Recieve email notifications when the job starts, finishes or fails.
-#SBATCH --mail-type=ALL
+##
+## Request Resources
+##
 
-# Set output directory to current
-#SBATCH --chdir=./
-
-# Specify the output file name
-#SBATCH -o openiss-reid-output-%A.log
-
-# Request Memory
 #SBATCH --mem=20G
-
-# Request CPU - comment this section if the job needs GPUs 
-##SBATCH -c 32
-
-# Request GPU - comment this section if the job needs CPUs and uncomment the previous section
+##SBATCH -c 32      ## Uncomment this line if the job needs CPUs and comment the GPU request section below
 #SBATCH --gpus=1
 
-# Execute the script
+# Load required modules
 module load anaconda3/2023.03/default
 
-# Define the environment directory
+# Define environment variables
 set ENV_DIR = /speed-scratch/$USER/reid-venv
 mkdir -p $ENV_DIR/{tmp,pkgs,cache}
 
-setenv TMPDIR $ENV_DIR/tmp
 setenv TMP $ENV_DIR/tmp
+setenv TMPDIR $ENV_DIR/tmp
 setenv CONDA_PKGS_DIRS $ENV_DIR/pkgs
 setenv PIP_CACHE_DIR $ENV_DIR/cache
 
+echo "Creating Conda environment at $ENV_DIR..."
+echo "===================================================="
 conda env create -f environment.yml -p $ENV_DIR >& conda-create.log
+
 conda activate $ENV_DIR
 
 date
+
+echo "Running reid.py..."
+echo "=================="
 srun python reid.py
+
 date
 
 conda deactivate
