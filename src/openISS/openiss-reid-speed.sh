1aba71245eedea8fcbac75b4158ede312c4b185b
Farah <49493059+salhanyf@users.noreply.github.com>
2025-08-25 16:00:13 -0400

    updated reid script to check for existing envs

--- src/openISS/openiss-reid-speed.sh
+++ src/openISS/openiss-reid-speed.sh
@@ -17,7 +17,10 @@
 module load anaconda3/2023.03/default
 
 # Define environment variables
-set ENV_DIR = /speed-scratch/$USER/reid-venv
+set ENV_NAME = reid-env
+set ENV_DIR = /speed-scratch/$USER/envs
+set ENV_PATH = $ENV_DIR/$ENV_NAME
+
 mkdir -p $ENV_DIR/{tmp,pkgs,cache}
 
 setenv TMP $ENV_DIR/tmp
@@ -25,18 +28,38 @@ setenv TMPDIR $ENV_DIR/tmp
 setenv CONDA_PKGS_DIRS $ENV_DIR/pkgs
 setenv PIP_CACHE_DIR $ENV_DIR/cache
 
-echo "Creating Conda environment at $ENV_DIR..."
-echo "===================================================="
-conda env create -f environment.yml -p $ENV_DIR >& conda-create.log
+# Check if the environment already exists
+if ( -d "$ENV_PATH" ) then
+    echo "Environment $ENV_NAME already exists at $ENV_PATH. Activating it..."
+    echo "======================================================================"
+    conda activate "$ENV_PATH"
+    if ($status != 0) then
+        echo "Error: Failed to activate Conda environment."
+        exit 1
+	endif
+else
+    echo "Creating Conda environment $ENV_NAME at $ENV_PATH..."
+    echo "======================================================================"
+    conda env create -f environment.yml -p $ENV_PATH |& tee conda-create.log
 
-conda activate $ENV_DIR
+    echo "Activating Conda environment $ENV_NAME..."
+	echo "======================================================================"
+    conda activate "$ENV_PATH"
+    if ($status != 0) then
+        echo "Error: Failed to activate Conda environment."
+        exit 1
+    endif            
+endif
 
 date
+echo "======================================================================"
 
 echo "Running reid.py..."
-echo "=================="
 srun python reid.py
 
+echo "======================================================================"
 date
 
+echo "================================ DONE ================================"
+echo "Deactivating Conda environment..."
 conda deactivate
